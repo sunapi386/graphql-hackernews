c1c0306d56633763aba1842aa7a44bce95ec6248
Jason Sun <jason@sunapi386.ca>
2018-10-28 00:17:33 -0700

    authentication

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import LinkList from "./LinkList";
+import Login from "./Login";
+import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return <LinkList />;
+    return (
+      <div className="center w85">
+        {/*<Header />*/}
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
